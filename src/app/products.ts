587624cdee1260f6e8fb1a10e8366f87e4766eca
lauracorssac <lauracorssac@gmail.com>
2021-07-20 22:04:00 -0300

    4 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,31 +1,17 @@
-export interface Product {
-  id: number;
-  name: string;
-  price: number;
-  description: string;
-}
-
-export const products = [
+export const recipes = [
   {
     id: 1,
-    name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens'
+    name: 'pacoca'
   },
   {
     id: 2,
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
+    name: 'brigadeiro'
   },
   {
     id: 3,
-    name: 'Phone Standard',
-    price: 299,
-    description: ''
+    name: 'feijao'
   }
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
