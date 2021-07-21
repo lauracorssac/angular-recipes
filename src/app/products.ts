806821ad381877f7e19eca329f2317097335c01b
lauracorssac <lauracorssac@gmail.com>
2021-07-21 20:10:48 -0300

    3 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -3,19 +3,22 @@ export const recipes = [
     id: 0,
     name: 'pacoca',
     imageURL:
-      'https://drive.google.com/uc?export=view&id=16P4KjNy4gqWEAb0DfFKMmUpZLR6ZN48i'
+      'https://drive.google.com/uc?export=view&id=16P4KjNy4gqWEAb0DfFKMmUpZLR6ZN48i',
+    description: "pacoca is gooood"
   },
   {
     id: 1,
     name: 'brigadeiro',
     imageURL:
-      'https://drive.google.com/uc?export=view&id=16P4KjNy4gqWEAb0DfFKMmUpZLR6ZN48i'
+      'https://drive.google.com/uc?export=view&id=16P4KjNy4gqWEAb0DfFKMmUpZLR6ZN48i',
+      description: "pacoca is gooood"
   },
   {
     id: 2,
     name: 'feijao',
     imageURL:
-      'https://drive.google.com/uc?export=view&id=16P4KjNy4gqWEAb0DfFKMmUpZLR6ZN48i'
+      'https://drive.google.com/uc?export=view&id=16P4KjNy4gqWEAb0DfFKMmUpZLR6ZN48i',
+      description: "pacoca is gooood"
   }
 ];
 
--- src/app/recipe-class.ts
+++ src/app/recipe-class.ts
@@ -2,4 +2,5 @@ export interface RecipeClass {
   id: number;
   name: string;
   imageURL: string;
+  description: string;
 }
