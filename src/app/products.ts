1c3a54fdb1807339b60f0ead1c3ff471bc7d2c1c
lauracorssac <lauracorssac@gmail.com>
2021-07-22 10:48:34 -0300

    4 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,24 +1,25 @@
 export const recipes = [
   {
     id: 0,
-    name: 'pacoca',
+    name: 'Paçoca',
     imageURL:
       'https://drive.google.com/uc?export=view&id=16P4KjNy4gqWEAb0DfFKMmUpZLR6ZN48i',
-    description: "pacoca is gooood"
+    description:
+      'Das ist eine normalerweise korkförmige Süßigkeit aus Erdnüsse, Zucker und Salz. Manche Rezepte können auch Mehl aus entweder Maniok oder Mais enthalten. Das ist ein weiteres sehr leckeres Gericht, das wir von den Ureinwohnern geerbt und adaptiert haben. Deswegen hat das Dessert diesen Name, der aus dem indigenen Wort "posok" kommt, das "zerbröckeln" bedeutet. Die Süßigkeit kann sehr einfach in den Supermärkten gekauft werden und es wird üblicherweise im Juni gegessen, da wir in diesem Monat manche Festen zu Ehren des Heiligen Johannes der Täufer.'
   },
   {
     id: 1,
     name: 'brigadeiro',
     imageURL:
       'https://drive.google.com/uc?export=view&id=16P4KjNy4gqWEAb0DfFKMmUpZLR6ZN48i',
-      description: "pacoca is gooood"
+    description: 'pacoca is gooood'
   },
   {
     id: 2,
     name: 'feijao',
     imageURL:
       'https://drive.google.com/uc?export=view&id=16P4KjNy4gqWEAb0DfFKMmUpZLR6ZN48i',
-      description: "pacoca is gooood"
+    description: 'pacoca is gooood'
   }
 ];
 
