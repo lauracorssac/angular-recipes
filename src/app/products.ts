cc9ac8799534c9ed2944bae5c349d06a46f7d288
lauracorssac <lauracorssac@gmail.com>
2021-07-22 11:01:47 -0300

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -9,17 +9,20 @@ export const recipes = [
   },
   {
     id: 1,
-    name: 'brigadeiro',
+    name: 'Bolo de Milho',
     imageURL:
       'https://drive.google.com/uc?export=view&id=16P4KjNy4gqWEAb0DfFKMmUpZLR6ZN48i',
-    description: 'pacoca is gooood'
+    description:
+      'Der Bolo de Milho, auch bekannt als Bolo de fubá, ist ein Kuchen, das aus Maismehl besteht. Der Mais, so wie der Maniok, ist ein wichtiges Element nicht nur für die brasilianische Ernährung, sondern auch für unsere Kultur. Dieses Getreide wurde schon vor mehr als 500 Jahren in indigenen Zerimonien verwendet und heutzutage spielt es noch eine große Rolle in unserer Gesellschaft. Das Maiskuchen ist eines von vielen typischen Rezepten mit Mais. Es ist sehr typisch, ihn bei den Junifesten zu essen, wann es die Geburt von dem Heiligen Johannes der Täufer gefeiert wird. Außer dem Maismehl besteht das Rezept auch aus den üblischen Zutaten von Kuchen, wie Weißmehl, Eier, Öl, Zucker, und Backpulver. Manche Rezepten auch benutzen Fenchel, Kokosraspeln und geriebenen Parmesankäse.'
   },
   {
     id: 2,
-    name: 'feijao',
+    name: 'Churrasco',
     imageURL:
       'https://drive.google.com/uc?export=view&id=16P4KjNy4gqWEAb0DfFKMmUpZLR6ZN48i',
-    description: 'pacoca is gooood'
+
+    description:
+      'Das Churrasco ist ein weiteres Kulturerbe der Ureinwohner und heutzutage ein Synonym von Treffen von Freunden in Südbrasilien. Es bedeutet Fleisch, das am Feuer gekocht wird. Es wird normalerweise in einem Churrasqueira zubereitet, die ähnlich wie ein Kamin funktioniert.\n\n<h1>Zubereitung</h1>\n\nUm ein Churrasco zuzubereiten, muss man zuerst ein Feuer in dem Churrasqueira machen. Dann muss man die Fleischstücke in die Bratspieße stecken. Danach muss man die Spitzen der Spieße in die kleinen Löcher, die sich an der Rückwand des Churrasqueira befinden. Man muss dann nur warten, bis das Fleisch gekocht ist. Bis dahin ist es üblich Knoblauchbrote oder Würste zu essen.\n\n<h1>Kuriositäten 👀</h1>\n\nSo populär es ist, dass es schwer ist, eine Haus im Süden ohne diese Einrichtung zu finden. Es ist auch typisch für die Länder, die den Süden umgeben, Uruguay und Argentinien, obwohl sie andere Arten haben, es zuzubereiten. Zum Beispiel ist es in Uruguay "Parilla" genannt und die "Parrillera" hat die gleiche Idee wie das Churrasqueira mit einigen Unterschieden.\n\nAußerdem sind vegetarische Varianten von Churrasco mehr und mehr populär und können unsere Kultur bewahren und gleichzeitig modernisieren. Sie bestehen normalerweise aus Gemüse, wie Kartoffeln und Paprika und auch einer Käsesorte, Queijo-Coalho, das gut zum Grillen passt.'
   }
 ];
 
