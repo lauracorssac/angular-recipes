d873662ab86ba46b951756bbe8fcc77f3c5288af
lauracorssac <lauracorssac@gmail.com>
2021-07-22 18:21:09 -0300

    3 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -4,25 +4,53 @@ export const recipes = [
     name: 'Paçoca',
     imageURL:
       'https://drive.google.com/uc?export=view&id=16P4KjNy4gqWEAb0DfFKMmUpZLR6ZN48i',
-    description:
-      'Das ist eine normalerweise korkförmige Süßigkeit aus Erdnüsse, Zucker und Salz. Manche Rezepte können auch Mehl aus entweder Maniok oder Mais enthalten. Das ist ein weiteres sehr leckeres Gericht, das wir von den Ureinwohnern geerbt und adaptiert haben. Deswegen hat das Dessert diesen Name, der aus dem indigenen Wort "posok" kommt, das "zerbröckeln" bedeutet. Die Süßigkeit kann sehr einfach in den Supermärkten gekauft werden und es wird üblicherweise im Juni gegessen, da wir in diesem Monat manche Festen zu Ehren des Heiligen Johannes der Täufer.'
+    descriptions: [
+      {
+        titlle: '',
+        description:
+          'Das ist eine normalerweise korkförmige Süßigkeit aus Erdnüsse, Zucker und Salz. Manche Rezepte können auch Mehl aus entweder Maniok oder Mais enthalten. Das ist ein weiteres sehr leckeres Gericht, das wir von den Ureinwohnern geerbt und adaptiert haben. Deswegen hat das Dessert diesen Name, der aus dem indigenen Wort "posok" kommt, das "zerbröckeln" bedeutet. Die Süßigkeit kann sehr einfach in den Supermärkten gekauft werden und es wird üblicherweise im Juni gegessen, da wir in diesem Monat manche Festen zu Ehren des Heiligen Johannes der Täufer.'
+      }
+    ],
+    nationalities: ['Brazil']
   },
   {
     id: 1,
     name: 'Bolo de Milho',
     imageURL:
-      'https://drive.google.com/uc?export=view&id=16P4KjNy4gqWEAb0DfFKMmUpZLR6ZN48i',
-    description:
-      'Der Bolo de Milho, auch bekannt als Bolo de fubá, ist ein Kuchen, das aus Maismehl besteht. Der Mais, so wie der Maniok, ist ein wichtiges Element nicht nur für die brasilianische Ernährung, sondern auch für unsere Kultur. Dieses Getreide wurde schon vor mehr als 500 Jahren in indigenen Zerimonien verwendet und heutzutage spielt es noch eine große Rolle in unserer Gesellschaft. Das Maiskuchen ist eines von vielen typischen Rezepten mit Mais. Es ist sehr typisch, ihn bei den Junifesten zu essen, wann es die Geburt von dem Heiligen Johannes der Täufer gefeiert wird. Außer dem Maismehl besteht das Rezept auch aus den üblischen Zutaten von Kuchen, wie Weißmehl, Eier, Öl, Zucker, und Backpulver. Manche Rezepten auch benutzen Fenchel, Kokosraspeln und geriebenen Parmesankäse.'
+      'https://drive.google.com/uc?export=view&id=1wK_q7iCoLwu10GMXBc9oFqk97MUCEHYi',
+    description: [
+      {
+        title: '',
+        description:
+          'Der Bolo de Milho, auch bekannt als Bolo de fubá, ist ein Kuchen, das aus Maismehl besteht. Der Mais, so wie der Maniok, ist ein wichtiges Element nicht nur für die brasilianische Ernährung, sondern auch für unsere Kultur. Dieses Getreide wurde schon vor mehr als 500 Jahren in indigenen Zerimonien verwendet und heutzutage spielt es noch eine große Rolle in unserer Gesellschaft. Das Maiskuchen ist eines von vielen typischen Rezepten mit Mais. Es ist sehr typisch, ihn bei den Junifesten zu essen, wann es die Geburt von dem Heiligen Johannes der Täufer gefeiert wird. Außer dem Maismehl besteht das Rezept auch aus den üblischen Zutaten von Kuchen, wie Weißmehl, Eier, Öl, Zucker, und Backpulver. Manche Rezepten auch benutzen Fenchel, Kokosraspeln und geriebenen Parmesankäse.'
+      }
+    ],
+    nationalities: ['Brazil']
   },
   {
     id: 2,
     name: 'Churrasco',
     imageURL:
-      'https://drive.google.com/uc?export=view&id=16P4KjNy4gqWEAb0DfFKMmUpZLR6ZN48i',
+      'https://drive.google.com/uc?export=view&id=18FwAxB2B-ZPrAHLQHZGw2DVGwjmfCrb2',
 
-    description:
-      'Das Churrasco ist ein weiteres Kulturerbe der Ureinwohner und heutzutage ein Synonym von Treffen von Freunden in Südbrasilien. Es bedeutet Fleisch, das am Feuer gekocht wird. Es wird normalerweise in einem Churrasqueira zubereitet, die ähnlich wie ein Kamin funktioniert.\n\n<h1>Zubereitung</h1>\n\nUm ein Churrasco zuzubereiten, muss man zuerst ein Feuer in dem Churrasqueira machen. Dann muss man die Fleischstücke in die Bratspieße stecken. Danach muss man die Spitzen der Spieße in die kleinen Löcher, die sich an der Rückwand des Churrasqueira befinden. Man muss dann nur warten, bis das Fleisch gekocht ist. Bis dahin ist es üblich Knoblauchbrote oder Würste zu essen.\n\n<h1>Kuriositäten 👀</h1>\n\nSo populär es ist, dass es schwer ist, eine Haus im Süden ohne diese Einrichtung zu finden. Es ist auch typisch für die Länder, die den Süden umgeben, Uruguay und Argentinien, obwohl sie andere Arten haben, es zuzubereiten. Zum Beispiel ist es in Uruguay "Parilla" genannt und die "Parrillera" hat die gleiche Idee wie das Churrasqueira mit einigen Unterschieden.\n\nAußerdem sind vegetarische Varianten von Churrasco mehr und mehr populär und können unsere Kultur bewahren und gleichzeitig modernisieren. Sie bestehen normalerweise aus Gemüse, wie Kartoffeln und Paprika und auch einer Käsesorte, Queijo-Coalho, das gut zum Grillen passt.'
+    description: [
+      {
+        title: '',
+        description:
+          'Das Churrasco ist ein weiteres Kulturerbe der Ureinwohner und heutzutage ein Synonym von Treffen von Freunden in Südbrasilien. Es bedeutet Fleisch, das am Feuer gekocht wird. Es wird normalerweise in einem Churrasqueira zubereitet, die ähnlich wie ein Kamin funktioniert.'
+      },
+      {
+        title: 'Zubereitung',
+        description:
+          'Um ein Churrasco zuzubereiten, muss man zuerst ein Feuer in dem Churrasqueira machen. Dann muss man die Fleischstücke in die Bratspieße stecken. Danach muss man die Spitzen der Spieße in die kleinen Löcher, die sich an der Rückwand des Churrasqueira befinden. Man muss dann nur warten, bis das Fleisch gekocht ist. Bis dahin ist es üblich Knoblauchbrote oder Würste zu essen.'
+      },
+      {
+        title: 'Kuriositäten 👀',
+        description:
+          'So populär es ist, dass es schwer ist, eine Haus im Süden ohne diese Einrichtung zu finden. Es ist auch typisch für die Länder, die den Süden umgeben, Uruguay und Argentinien, obwohl sie andere Arten haben, es zuzubereiten. Zum Beispiel ist es in Uruguay "Parilla" genannt und die "Parrillera" hat die gleiche Idee wie das Churrasqueira mit einigen Unterschieden. Außerdem sind vegetarische Varianten von Churrasco mehr und mehr populär und können unsere Kultur bewahren und gleichzeitig modernisieren. Sie bestehen normalerweise aus Gemüse, wie Kartoffeln und Paprika und auch einer Käsesorte, Queijo-Coalho, das gut zum Grillen passt.'
+      }
+    ],
+    nationalities: ['Brazil, Argentina, Uruguay']
   }
 ];
 
--- src/app/recipe-class.ts
+++ src/app/recipe-class.ts
@@ -2,5 +2,10 @@ export interface RecipeClass {
   id: number;
   name: string;
   imageURL: string;
+  descriptions: [RecipeDescription];
+  nationalities: [string];
+}
+export interface RecipeDescription {
+  title: string;
   description: string;
 }
